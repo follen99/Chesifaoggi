99bb57bc35f1d51f2b05202152d7bb9ae7f02bc4
H M <AccaEmme@users.noreply.github.com>
2022-04-10 22:24:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Giuliano è un ladro, ha rubato il progetto Che si fa oggi e io sono rancoroso</Text>
       <StatusBar style="auto" />
     </View>
   );
